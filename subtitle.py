a8484fe36262d74b9717f68976bce4cadc090467
akash-uchiha <akash.minato@gmail.com>
2017-03-20 13:40:57 +0530

    Update subtitle.py

--- subtitle.py
+++ subtitle.py
@@ -1,13 +1,3 @@
-import os,  time
-
-filename='srt.txt'
-
-
-file = open(filename, "r")
-
-def caltime(str):
-    #returns time in seconds
-    return 3600*int(str[0])+60*int(str[1])+int(str[0:2])+int(str[3:])/1000.0
 
 
 #create list of all lines
@@ -15,15 +5,17 @@ lines=file.readlines()
 
 #remove \n items from list lines
 newlines=[]
-while line in lines:
+for line in lines:
     if line!='\n':
         newlines.append(line)
         
 clock=time.clock()       
 counter=1
+
+print "<start>"
 for x in range(0,len(newlines)) :
     
-    if  newlines[x]==string(counter)+'\n':
+    if  newlines[x]==str(counter)+'\n':
         tim=newlines[x+1]
         times=tim.split(' --> ')
         time1=times[0].split(':')
@@ -32,10 +24,10 @@ for x in range(0,len(newlines)) :
         t2=caltime(time2)
         delay=t2-t1
         
-        while(t1>time.clock())
+        while(t1>time.clock()):
             continue
         i=x+2    
-        while newlines[i]!=string(counter+1)+'\n':    
+        while newlines[i]!=str(counter+1)+'\n':    
             print newlines[i]
             i=i+1
         time.sleep(delay)
